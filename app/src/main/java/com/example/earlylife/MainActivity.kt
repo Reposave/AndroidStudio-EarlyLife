463ea8c30725ab87b5031fd4a6deb373c96be9e7
Tinashe Chitamba <chttin007@cs.uct.ac.za>
2021-08-23 22:20:36 +0200

    Got anychart to work within the app

--- app/src/main/java/com/example/earlylife/MainActivity.kt
+++ app/src/main/java/com/example/earlylife/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.earlylife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Creating a dummy pie chart to test
+        val pie = AnyChart.pie()
+        val data = ArrayList<DataEntry>()
+        data.add(ValueDataEntry("Love",100))
+        data.add(ValueDataEntry("Numbers",200))
+        data.add(ValueDataEntry("Shapes",300))
+        pie.data(data)
+
+        val anyChartView:AnyChartView = findViewById(R.id.any_chart_view)
+        anyChartView.setChart(pie)
     }
 }
