38399c8f69d02d9dbb255e3a90cfbd1534aa8279
Tinashe Chitamba <chttin007@cs.uct.ac.za>
2021-08-24 17:26:25 +0200

    Added a new activity for an individual activity report, only works for only 1 button though since it's still hard coded

---
+++ app/src/main/java/com/example/earlylife/ActivityReport.kt
@@ -0,0 +1,65 @@
+package com.example.earlylife
+
+import android.graphics.Point
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.anychart.APIlib
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import java.util.*
+
+
+class ActivityReport : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+        var anyChartView: AnyChartView = findViewById(R.id.any_chart_view)
+        APIlib.getInstance().setActiveAnyChartView(anyChartView)
+
+        //Adding a progress meter
+        val successMeter = AnyChart.pie()
+        successMeter.innerRadius("80%")
+
+        val data = ArrayList<DataEntry>()
+        data.add(ValueDataEntry("Correct", 80))
+        data.add(ValueDataEntry("Incorrect", 20))
+        successMeter.data(data)
+
+        successMeter.label("Success Rate")
+        successMeter.labels().position("center-top")
+
+        //making the view half the size of the parent
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val s_width: Int = size.x
+
+        //Adding the chart to the UI
+        anyChartView.layoutParams.width = s_width/2
+        anyChartView.setChart(successMeter)
+
+
+        //adding the report details width to the UI
+        val activityDetails = findViewById<View>(R.id.activity_details)
+        activityDetails.layoutParams.width = s_width/2
+
+        val anyChartLineView: AnyChartView = findViewById(R.id.any_chart_line_view)
+        APIlib.getInstance().setActiveAnyChartView(anyChartLineView)
+        //adding the line graph
+        val lineGraph = AnyChart.line()
+
+        val lineData = ArrayList<DataEntry>()
+        lineData.add(ValueDataEntry("Sunday",3))
+        lineData.add(ValueDataEntry("Monday",5))
+        lineData.add(ValueDataEntry("Wednesday",1))
+        lineData.add(ValueDataEntry("Thursday",2))
+        lineData.add(ValueDataEntry("Friday",0))
+        lineData.add(ValueDataEntry("Saturday",2))
+        lineGraph.data(lineData)
+        anyChartLineView.setChart(lineGraph)
+
+    }
+}
--- app/src/main/java/com/example/earlylife/MainActivity.kt
+++ app/src/main/java/com/example/earlylife/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.earlylife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,6 +10,7 @@ import com.anychart.AnyChartView
 import com.anychart.chart.common.dataentry.DataEntry
 import com.anychart.chart.common.dataentry.ValueDataEntry
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,5 +28,10 @@ class MainActivity : AppCompatActivity() {
         //Adding the chart to the UI
         val anyChartView:AnyChartView = findViewById(R.id.any_chart_view)
         anyChartView.setChart(pie)
+    }
+
+    fun startActivityReport(view: View){
+        val intent = Intent(this,ActivityReport::class.java)
+        startActivity(intent)
     }
 }
