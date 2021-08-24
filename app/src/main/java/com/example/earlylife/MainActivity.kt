8dcdcf0fa1f78506bf3091b18d4a363c4b8b7b9d
Tinashe Chitamba <chttin007@cs.uct.ac.za>
2021-08-24 12:56:10 +0200

    Constrained the pie chart to a layout view, added another layout view and added buttons for each activity

--- app/src/main/java/com/example/earlylife/MainActivity.kt
+++ app/src/main/java/com/example/earlylife/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.earlylife
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.anychart.AnyChart
 import com.anychart.AnyChartView
 import com.anychart.chart.common.dataentry.DataEntry
@@ -14,15 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         //Creating a dummy pie chart to test
         val pie = AnyChart.pie()
+        //Array with the data
         val data = ArrayList<DataEntry>()
         data.add(ValueDataEntry("Love",100))
         data.add(ValueDataEntry("Numbers",200))
         data.add(ValueDataEntry("Shapes",300))
         pie.data(data)
-
+        //Adding the chart to the UI
         val anyChartView:AnyChartView = findViewById(R.id.any_chart_view)
         anyChartView.setChart(pie)
     }
