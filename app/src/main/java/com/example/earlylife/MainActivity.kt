abaf064821340ac0dbd68d884f1b1f59c2696acb
Tinashe Chitamba <chttin007@cs.uct.ac.za>
2021-08-31 16:41:16 +0200

    Can now change chart types, line and bar

--- app/src/main/java/com/example/earlylife/MainActivity.kt
+++ app/src/main/java/com/example/earlylife/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.NavHostFragment
@@ -22,31 +23,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val lineChartButton = findViewById<View>(R.id.line_fragment_button)
+        val barChartButton = findViewById<View>(R.id.bar_fragment_button)
+
+        lineChartButton.setOnClickListener { changeChartType(LineChartFragment()) }
+        barChartButton.setOnClickListener { changeChartType(ReportBarChart()) }
+
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction =fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.bar_chart_fragment, ReportBarChart())
         fragmentTransaction.commit()
-        /*
-        //val mDBManager = DBManager(this.application)
-        //Creating a dummy pie chart to test
-        val pie = AnyChart.pie()
-        //Array with the data
-        val data = ArrayList<DataEntry>()
 
-
-        data.add(ValueDataEntry("Love",100))
-        data.add(ValueDataEntry("Numbers",300))
-        data.add(ValueDataEntry("Shapes",50))
-
-        pie.data(data)
-        //Adding the chart to the UI
-        val anyChartView:AnyChartView = findViewById(R.id.any_chart_view)
-        anyChartView.setChart(pie)
-        */
     }
 
     fun startActivityReport(view: View){
         val intent = Intent(this,ActivityReport::class.java)
         startActivity(intent)
+    }
+
+    fun changeChartType(fragment: Fragment){
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction =fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.bar_chart_fragment, fragment)
+        fragmentTransaction.commit()
+
     }
 }
