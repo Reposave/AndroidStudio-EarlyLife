c3911aef048ec9c0faaf493e5147001d132ed658
Ardo Dlamini <dlmard001@cs.uct.ac.za>
2021-09-15 14:30:50 +0200

    Added connection instructions to ConnectToQuilt screen.

--- app/src/main/java/com/example/earlylife/ConnectToQuilt.kt
+++ app/src/main/java/com/example/earlylife/ConnectToQuilt.kt
@@ -1,11 +1,19 @@
 package com.example.earlylife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ConnectToQuilt : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_to_quilt)
+        var btnWifiConnect = findViewById<View>(R.id.btn_WifiConnect)
+
+        btnWifiConnect.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
