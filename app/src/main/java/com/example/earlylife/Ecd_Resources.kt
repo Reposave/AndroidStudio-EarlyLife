4e656601e6d2eb634043ef033cea03b0afc17922
Samoei <samoeiayuka@gmail.com>
2021-09-17 03:30:50 +0200

    Declared ListView in layout

--- app/src/main/java/com/example/earlylife/Ecd_Resources.kt
+++ app/src/main/java/com/example/earlylife/Ecd_Resources.kt
@@ -8,9 +8,16 @@ class Ecd_Resources : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecd__resources)
+
+        // Define list of ECD resources
+        val arrayAdapter: ArrayAdapter<String>
+        val resources = arrayOf("Resource 1", "Resource 2", "Resource 3", "Resource 4")
+
+        // Access ListView from XML file
+        var listView = findViewById<ListView>(R.id.userlist)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, resources)
+        listView.adapter = arrayAdapter
     }
 
-    // Define list of ECD resources
-    val arrayAdapter: ArrayAdapter<*>
-    val resources = arrayOf("Resource 1", "Resource 2", "Resource 3", "Resource 4")
+
 }
