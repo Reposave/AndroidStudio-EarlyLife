f6111c8944ff493ef088d3697148b200757cf930
Samoei <samoeiayuka@gmail.com>
2021-09-17 02:54:21 +0200

    Creating ListView for ECD resources

--- app/src/main/java/com/example/earlylife/Ecd_Resources.kt
+++ app/src/main/java/com/example/earlylife/Ecd_Resources.kt
@@ -1,5 +1,6 @@
 package com.example.earlylife
-
+import android.widget.ListView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,4 +9,8 @@ class Ecd_Resources : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecd__resources)
     }
+
+    // Define list of ECD resources
+    val arrayAdapter: ArrayAdapter<*>
+    val resources = arrayOf("Resource 1", "Resource 2", "Resource 3", "Resource 4")
 }
