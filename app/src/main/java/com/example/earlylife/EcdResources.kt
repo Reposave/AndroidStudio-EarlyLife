11f48c3ae8c940ba094c0bab33191763709d5a43
Samoei <samoeiayuka@gmail.com>
2021-10-03 14:06:44 +0200

    Added cards for resources

--- app/src/main/java/com/example/earlylife/EcdResources.kt
+++ app/src/main/java/com/example/earlylife/EcdResources.kt
@@ -1,5 +1,6 @@
 package com.example.earlylife
 import android.content.Context
+import android.content.Intent
 import android.widget.ListView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
@@ -8,18 +9,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 class EcdResources : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecd__resources)
         //Access ListView from XML file
-        val listView = findViewById<ListView>(R.id.main_listview)
-
+        //val listView = findViewById<ListView>(R.id.main_listview)
+        val cardView = findViewById<CardView>(R.id.card_view1)
+        cardView.setOnClickListener(clickListener)
 
         // Adapter tells list what to render
-        listView.adapter = CustomAdapter(this)
+        //listView.adapter = CustomAdapter(this)
 
        /** // Define list of ECD resources
         val arrayAdapter: ArrayAdapter<String>
@@ -31,11 +35,22 @@ class EcdResources : AppCompatActivity() {
         listView.adapter = arrayAdapter*/
     }
 
+    private val clickListener: View.OnClickListener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.card_view1 -> learnToPlay()
+        }
+    }
+
+    private fun learnToPlay() {
+        val intent = Intent(this, LearnToPlay::class.java)
+        startActivity(intent)
+    }
+
 
 
 
 }
-
+/*
 private class CustomAdapter(context : Context): BaseAdapter(){
 
     private val aContext : Context
@@ -63,4 +78,4 @@ private class CustomAdapter(context : Context): BaseAdapter(){
         textView.text = "HERE is my ROW for my LISTVIEW"
         return textView
     }
-}
+}*/
