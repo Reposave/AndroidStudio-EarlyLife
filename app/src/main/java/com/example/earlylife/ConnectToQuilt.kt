046c7c6eaf8aa86928e43ddee5c507f39395f5b2
Ardo Dlamini <dlmard001@cs.uct.ac.za>
2021-10-03 19:23:25 +0200

    Added Downloading of data after connection.

--- app/src/main/java/com/example/earlylife/ConnectToQuilt.kt
+++ app/src/main/java/com/example/earlylife/ConnectToQuilt.kt
@@ -1,5 +1,6 @@
 package com.example.earlylife
 
+import android.content.ContentValues
 import android.content.Intent
 import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,15 @@ import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_connect_to_quilt.view.*
 import android.net.wifi.WifiInfo
+import android.provider.BaseColumns
+import android.util.Log
+import android.widget.TextView
+import com.example.earlylife.Models.Quilt
+import com.example.earlylife.Retrofit.RetrofitService
+import com.example.earlylife.SQLite.FeedReaderContract
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 @Suppress("DEPRECATION")
 
@@ -60,22 +70,32 @@ class ConnectToQuilt : AppCompatActivity() {
             wifiManager.enableNetwork(netId, true)
             wifiManager.reconnect()
 
+            //Find a way to create a delay before running this next section of code.
+            var i = 0
+            while(i == 0) {
+                if (wifiManager.isWifiEnabled) { // Wi-Fi adapter is ON
+                    val wifiInfo = wifiManager.connectionInfo
+                    if (wifiInfo.networkId == -1) {
+                        Toast.makeText(this, "SmartQuilt network not found.", Toast.LENGTH_LONG)
+                            .show();
+                        instructText.textView2.text = getString(R.string.instructions3)
+                        // Not connected to an access point
+                    } else {
+                        i = 1
+                        Toast.makeText(this, "Connected to a network.", Toast.LENGTH_LONG).show();
+                        //Add download code.
+                        DownloadData()
+                        instructText.textView2.text = getString(R.string.instructions5)
 
-            if (wifiManager.isWifiEnabled) { // Wi-Fi adapter is ON
-                val wifiInfo = wifiManager.connectionInfo
-                if (wifiInfo.networkId == -1) {
-                    Toast.makeText(this, "SmartQuilt network not found.", Toast.LENGTH_LONG).show();
-                    instructText.textView2.text = getString(R.string.instructions3)
-                    false // Not connected to an access point
+                        //Add delay.
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    // Connected to an access point
                 } else {
-                    Toast.makeText(this, "Connected to a network.", Toast.LENGTH_LONG).show();
-                    //Add download code.
-                    true
+                    instructText.textView2.text = getString(R.string.instructions4)
+                    // Wi-Fi adapter is OFF
                 }
-                // Connected to an access point
-            } else {
-                instructText.textView2.text = getString(R.string.instructions4)
-                false // Wi-Fi adapter is OFF
             }
             //checkWifiOnAndConnected()
 
@@ -126,5 +146,96 @@ class ConnectToQuilt : AppCompatActivity() {
             }
         }
     }
+    private fun DownloadData(){
+        val compositeDisposable = CompositeDisposable()
+        compositeDisposable.add(
+            RetrofitService.ServiceBuilder.buildService().getShapes()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({response -> onResponse(response)}, {t -> onFailure(t) }))
+    }
 
+    private fun onFailure(t: Throwable) {
+        Toast.makeText(this,t.message, Toast.LENGTH_SHORT).show()
+        var txt_activityID = findViewById<TextView>(R.id.sensor_data)
+        Log.d("ERROR",t.toString())
+
+    }
+
+    private fun onResponse(response: Quilt) {
+        Log.d("Response",response.toString())
+
+        //saving to database
+        val dbHelper = FeedReaderContract.FeedReaderDbHelper(this.applicationContext)
+        // Gets the data repository in write mode
+        val db = dbHelper.writableDatabase
+
+        // Create a new map of values, where column names are the keys
+        val learnShapesValues = ContentValues().apply {
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_ID, response.LearnShapes.activityID)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_NAME, response.LearnShapes.acticityName)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_DATE, response.LearnShapes.date)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_TIME_ON_TASK, response.LearnShapes.timeOnTask)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_CORRECT, response.LearnShapes.correct)
+        }
+
+        val learnNumbersValues = ContentValues().apply {
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_ID, response.LearnShapes.activityID)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_NAME, response.LearnShapes.acticityName)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_DATE, response.LearnShapes.date)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_TIME_ON_TASK, response.LearnShapes.timeOnTask)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_CORRECT, response.LearnShapes.correct)
+        }
+        /*
+        val loveValues = ContentValues().apply {
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_ID, response.Love.activityID)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_NAME, response.Love.acticityName)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_DATE, response.Love.date)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_TIME_ON_TASK, response.Love.timeOnTask)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_CORRECT, response.Love.correct)
+        }
+        val matchShapesValues = ContentValues().apply {
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_ID, response.MarchShapes.activityID)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_NAME, response.MarchShapes.acticityName)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_DATE, response.MarchShapes.date)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_TIME_ON_TASK, response.MarchShapes.timeOnTask)
+            put(FeedReaderContract.FeedEntry.COLUMN_NAME_CORRECT, response.MarchShapes.correct)
+        }
+
+ */
+
+        Log.d("Debug","Values created")
+        // Insert the new row, returning the primary key value of the new row
+        var newRowId = db?.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, learnShapesValues)
+        //newRowId = db?.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, loveValues)
+
+        //reading the values from database
+        val dbr = dbHelper.readableDatabase
+        val projection = arrayOf(
+            BaseColumns._ID, FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_ID,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_NAME,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_TIME_ON_TASK,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_CORRECT,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_DATE)
+
+        val cursor = dbr.query(
+            FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+            projection,             // The array of columns to return (pass null to get all)
+            null,              // The columns for the WHERE clause
+            null,          // The values for the WHERE clause
+            null,                   // don't group the rows
+            null,                   // don't filter by row groups
+            null               // The sort order
+        )
+
+        val itemIds = mutableListOf<String>()
+        with(cursor) {
+            while (moveToNext()) {
+                val itemId = getString(getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_DATE))
+                itemIds.add(itemId)
+            }
+        }
+        cursor.close()
+        Log.e("Database", itemIds.toString())
+    }
 }
