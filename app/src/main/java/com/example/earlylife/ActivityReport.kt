2bc7b8cf829578e2541b593775cb9ad2f4a2fc8a
Tinashe Chitamba <chttin007@cs.uct.ac.za>
2021-10-04 16:08:41 +0200

    can now start individual activity with name

--- app/src/main/java/com/example/earlylife/ActivityReport.kt
+++ app/src/main/java/com/example/earlylife/ActivityReport.kt
@@ -2,13 +2,17 @@ package com.example.earlylife
 
 import android.graphics.Point
 import android.os.Bundle
+import android.provider.BaseColumns
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.anychart.APIlib
 import com.anychart.AnyChart
 import com.anychart.AnyChartView
 import com.anychart.chart.common.dataentry.DataEntry
 import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.example.earlylife.SQLite.FeedReaderContract
 import java.util.*
 
 
@@ -16,6 +20,15 @@ class ActivityReport : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+        var activityName : String? = null
+        intent.extras?.getString("ActivityName")?.let { Log.d("debug", it) }
+        var extras = intent.extras
+        if (extras != null) {
+            activityName = extras.getString("ActivityName").toString()
+        }
+
+        var details = findViewById<TextView>(R.id.activity_details_label)
+        details.setText("The activity is "+activityName)
 
 
         var anyChartView: AnyChartView = findViewById(R.id.any_chart_view)
@@ -55,5 +68,49 @@ class ActivityReport : AppCompatActivity() {
         anyChartLineView.setChart(lineGraph)
 
 
+    }
+
+    fun getCorrect(activityName: String, txtView: TextView?): Int{
+        var correct = 0
+        val dbHelper =  FeedReaderContract.FeedReaderDbHelper(this.applicationContext)
+        val dbr = dbHelper?.readableDatabase
+        val projection = arrayOf(
+            BaseColumns._ID, FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_ID,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_NAME,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_TIME_ON_TASK,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_CORRECT,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_DATE)
+
+        var selection = "${FeedReaderContract.FeedEntry.COLUMN_NAME_ACTIVITY_NAME} = ?"
+        val cursor = dbr?.query(
+            FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+            projection,             // The array of columns to return (pass null to get all)
+            selection,              // The columns for the WHERE clause
+            arrayOf(activityName),          // The values for the WHERE clause
+            null,                // don't group the rows
+            null,                   // don't filter by row groups
+            null               // The sort order
+        )
+
+        val shapesStats = mutableListOf<Int>()
+        with(cursor) {
+            while (this?.moveToNext() == true) {
+                val itemId = getInt(getColumnIndexOrThrow(com.example.earlylife.SQLite.FeedReaderContract.FeedEntry.COLUMN_NAME_CORRECT))
+                shapesStats.add(itemId)
+            }
+        }
+        if (cursor != null) {
+            cursor.close()
+        }
+        Log.d("Debug", shapesStats.toString())
+        for(a in shapesStats){
+            if (a != null) {
+                correct += a //Integer.getInteger(a)
+            }
+        }
+        if (txtView != null) {
+            txtView.setText(correct.toString())
+        }
+        return correct
     }
 }
